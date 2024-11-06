1b2bab0263118eb071e080a7721fc73e1ecfe8f0
mehran-bangash <221029@students.au.edu.pk>
2024-11-06 09:36:26 +0500

    Order code uploaded

---
+++ order-service/src/main/com/example/OrderController.java
@@ -0,0 +1,24 @@
+package com.example;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/orders")
+public class OrderController {
+
+    @Autowired
+    private OrderRepository orderRepository;
+
+    @PostMapping
+    public Order createOrder(@RequestBody Order order) {
+        return orderRepository.save(order);
+    }
+
+    @GetMapping
+    public List<Order> getOrders() {
+        return orderRepository.findAll();
+    }
+}
